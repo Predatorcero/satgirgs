901397c198ba2f76cc9a7ac16f7ff5cea93b29e7
Predatorcero <nicolakoessler@gmx.de>
2023-04-25 18:07:56 +0200

    added dimension parameter for satgirgs

--- source/cli/gensatgirg/main.cpp
+++ source/cli/gensatgirg/main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char* argv[]) {
     if (argc < 2 || 0 == strcmp(argv[1], "--help") || 0 == strcmp(argv[1], "-help")) {
         clog << "usage: ./gensatgirg\n"
              << "\t\t[-n anInt]          // number of vertices (non-clause points)   default 10000\n"
+             << "\t\t[-d anInt]          // dimension of geometry    range [1,5]     default 2\n"
              << "\t\t[-m anInt]          // number of edges (clause points)          default 10000\n"
              << "\t\t[-k anInt]          // number literals per clause               default 2\n"
              << "\t\t[-ple aFloat]       // power law exponent       range (2,3]     default 2.5\n"
@@ -85,6 +86,7 @@ int main(int argc, char* argv[]) {
     // read params
     auto params = parseArgs(argc, argv);
     auto n      = !params["n"    ].empty()  ? stoi(params["n"    ]) : 10000;
+    auto d      = !params["d"    ].empty()  ? stoi(params["d"    ]) : 2;
     auto m      = !params["m"    ].empty()  ? stoi(params["m"    ]) : 10000; // TODO find sensible default and change in usage above
     auto k      = !params["k"    ].empty()  ? stoi(params["k"    ]) : 2; // TODO find sensible default and change in usage above
     auto ple    = !params["ple"  ].empty()  ? stod(params["ple"  ]) : 2.5;
@@ -130,14 +132,14 @@ int main(int argc, char* argv[]) {
 
 
     cout << "generating non-clause positions ...\t" << flush;
-    auto nc_positions = satgirgs::generatePositions(n, 2, ncseed);
+    auto nc_positions = satgirgs::generatePositions(n, d, ncseed);
     auto nc_nodes = satgirgs::convertToNodes(nc_positions, weights);
     auto t3 = high_resolution_clock::now();
     cout << "done in " << duration_cast<milliseconds>(t3 - t2).count() << "ms" << endl;
 
 
     cout << "generating clause positions ...\t\t" << flush;
-    auto c_positions = satgirgs::generatePositions(m, 2, cseed);
+    auto c_positions = satgirgs::generatePositions(m, d, cseed);
     std::vector<double> c_pseudoweights(m, 1); // clause nodes all have weight 1 in the model
     auto c_nodes = satgirgs::convertToNodes(c_positions, c_pseudoweights, nc_nodes.size());
     auto t4 = high_resolution_clock::now();
